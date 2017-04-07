f1d7440e3ef784f7d856f5b64a786bf6d92de9a8
kpatrik <patrik5000@gmail.com>
2017-04-06 23:52:28 -0700

    refoctor code

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+    if(firingMode == SINGLE){
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,16 +73,16 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
+       }
 
-      case ALL:
+      else{
         // try to fire both of the torpedos
        if(! primaryTorpedoStore.isEmpty() && ! secondaryTorpedoStore.isEmpty() ){
 		 primaryTorpedoStore.fire(1);
 		 firingSuccess =  secondaryTorpedoStore.fire(1);
 	}
 
-        break;
+      }
     }
 
     return firingSuccess;
